44df50e7461b6baa947f2257deae1adacb203aba
Yorrick Bakker <y.bakker@me.com>
2019-10-17 11:12:44 +0200

    Cleaning up react app

--- quizzer-client/src/index.jsx
+++ quizzer-client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'css/main.css';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
