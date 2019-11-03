275fcb9f53f74dd28e5efc62272d52b1ed9a6b20
Yorrick Bakker <y.bakker@me.com>
2019-11-03 18:14:16 +0100

    Allowing quizmaster to update the question

---
+++ quizzer-client/src/actions/async/fetchQuestionsAction.js
@@ -0,0 +1,43 @@
+import * as fetchState from '../fetchStateActions'
+import * as quizState from '../quizStateActions'
+
+import * as GLOBALS from '../../globals'
+
+export default function fetchQuestionsAction(amt = 10) {
+  return dispatch => {
+    dispatch(fetchState.updateFetchingAction(true))
+
+    fetch(`${GLOBALS.API_URL}/questions/random?amount=${amt}`, {
+      method: 'GET',
+      cache: 'no-cache',
+      credentials: 'include',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json()
+        .then(parsed => {
+          if (!res.ok) throw parsed
+          dispatch(fetchState.updateFetchingAction(false))
+
+          dispatch(quizState.setPickableQuestionsAction(parsed.questions))
+        })
+        .catch(parsed => {
+          const { error } = parsed
+          dispatch(fetchState.updateFetchingResultAction(null))
+          dispatch(fetchState.updateFetchingAction(false))
+          dispatch(fetchState.updateFetchingResultAction('error'))
+          dispatch(fetchState.updateFetchingMessageAction(error))
+        })
+      )
+      .catch(err => {
+        console.log('fetch error: ', err)
+        dispatch(fetchState.updateFetchingResultAction(null))
+        dispatch(fetchState.updateFetchingAction(false))
+        dispatch(fetchState.updateFetchingResultAction('error'))
+        dispatch(fetchState.updateFetchingMessageAction('Couldn\'t fetch from API'))
+      })
+  }
+}
---
+++ quizzer-client/src/actions/async/setQuestionAction.js
@@ -0,0 +1,52 @@
+import * as fetchState from '../fetchStateActions'
+import * as quizState from '../quizStateActions'
+
+import { history } from '../../containers/quizzer'
+
+import * as GLOBALS from '../../globals'
+
+export default function setQuestionAction(q) {
+  return (dispatch, getState) => {
+    dispatch(fetchState.updateFetchingAction(true))
+
+    const { currentRoomNumber } = getState().appState
+
+    fetch(`${GLOBALS.API_URL}/rooms/${currentRoomNumber}/round`, {
+      method: 'PATCH',
+      cache: 'no-cache',
+      credentials: 'include',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        questionId: q
+      })
+    })
+      .then(res => res.json()
+        .then(parsed => {
+          if (!res.ok) throw parsed
+          dispatch(fetchState.updateFetchingAction(false))
+          dispatch(quizState.setQuestionAction(parsed.question))
+          dispatch(quizState.setQuestionNrAction(parsed.questionNr))
+          dispatch(quizState.setAnswerAction(parsed.action))
+          // history.push('/quizmaster/verifyquestions')
+        })
+        .catch(parsed => {
+          const { error } = parsed
+          dispatch(fetchState.updateFetchingResultAction(null))
+          dispatch(fetchState.updateFetchingAction(false))
+          dispatch(fetchState.updateFetchingResultAction('error'))
+          dispatch(fetchState.updateFetchingMessageAction(error))
+        })
+      )
+      .catch(err => {
+        console.log('fetch error: ', err)
+        dispatch(fetchState.updateFetchingResultAction(null))
+        dispatch(fetchState.updateFetchingAction(false))
+        dispatch(fetchState.updateFetchingResultAction('error'))
+        dispatch(fetchState.updateFetchingMessageAction('Couldn\'t fetch from API'))
+      })
+  }
+}
