895ffbad73f67be4ede796541a620ea99168473e
Yorrick Bakker <y.bakker@me.com>
2020-04-26 12:48:01 +0200

    Replace hard coded env variables with env variables

--- quizzer-client/src/actions/async/defaultFetch.js
+++ quizzer-client/src/actions/async/defaultFetch.js
@@ -1,32 +1,35 @@
-import * as GLOBALS from '../../globals'
+import * as GLOBALS from '../../globals';
 
 /**
- * 
+ *
  * @param {*} api_path path to the quizzer api resource
  * @param {*} method method used for the fetch
  * @param {*} body stringified JSON body
- * defaultFetch returns a promise with 
+ * defaultFetch returns a promise with
  * {APIerr: statuscode, data}
  */
 const defaultFetch = (api_path, method, body) => {
-  return fetch(`${GLOBALS.API_URL}${api_path}`, {
-    ...GLOBALS.FETCH_OPTIONS,
-    method: method,
-    body: body
-  })
-    // Fetch went ok, server is up
-    .then(res => {
-      return res.json()
-        .then(parsed => {
-          if (!res.ok) return { APIerr: res.status ? res.status : true, data: parsed }
-          else return { data: parsed }
-        })
+  console.log(GLOBALS.API_URL);
+  return (
+    fetch(`${GLOBALS.API_URL}${api_path}`, {
+      ...GLOBALS.FETCH_OPTIONS,
+      method: method,
+      body: body,
     })
-    // Server might be down!
-    .catch(err => {
-      console.log(err)
-      return Promise.reject("The API is not responding!")
-    })
-}
+      // Fetch went ok, server is up
+      .then((res) => {
+        return res.json().then((parsed) => {
+          if (!res.ok)
+            return { APIerr: res.status ? res.status : true, data: parsed };
+          else return { data: parsed };
+        });
+      })
+      // Server might be down!
+      .catch((err) => {
+        console.log(err);
+        return Promise.reject('The API is not responding!');
+      })
+  );
+};
 
-export default defaultFetch
+export default defaultFetch;
