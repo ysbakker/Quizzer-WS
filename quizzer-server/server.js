1ed19951a504ddf4e50048c2b0464514ef184c23
Yorrick Bakker <y.bakker@me.com>
2020-04-26 12:51:32 +0200

    Put SESSION_SECRET in .env (it's changed now :D)

--- quizzer-server/server.js
+++ quizzer-server/server.js
@@ -1,48 +1,47 @@
-const http = require('http')
-const cors = require('cors')
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const wss = require('./socket')
+require('dotenv').config();
+const http = require('http');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const wss = require('./socket');
 
 const app = express();
 const server = http.createServer();
 
 const sessionParser = session({
-  secret: '9jDb8AHQdY',
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   store: new MongoStore({
-    mongooseConnection: mongoose.connection
-  })
-})
+    mongooseConnection: mongoose.connection,
+  }),
+});
 
 /*********************************
  * Upgrade to WebSocket Protocol *
  *********************************/
 
-server.on("upgrade", (request, socket, head) => {
+server.on('upgrade', (request, socket, head) => {
   sessionParser(request, {}, () => {
-    const {
-      session
-    } = request
+    const { session } = request;
 
     if (session.auth === 1) {
-      wss.handleUpgrade(request, socket, head, socket => {
+      wss.handleUpgrade(request, socket, head, (socket) => {
         wss.emit('connection', socket, request);
-      })
-      return
+      });
+      return;
     }
 
-    socket.destroy()
-  })
-})
+    socket.destroy();
+  });
+});
 
 server.on('request', app);
 server.listen(3000, () => {
-  console.log("The Server is lisening on port 3000.")
+  console.log('The Server is lisening on port 3000.');
 });
 
 /*******************
@@ -52,29 +51,34 @@ server.listen(3000, () => {
 
 mongoose.connect('mongodb://127.0.0.1/Quizzer', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-app.options("*", cors({
-  origin: true,
-  credentials: true
-}));
+app.options(
+  '*',
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(sessionParser);
 
 // Attach API routes
-app.use('/quizzer', require('./routes/index'))
+app.use('/quizzer', require('./routes/index'));
 
 // Error handling
 app.use((err, req, res, next) => {
-  res.status(err.rescode !== undefined ? err.rescode : 418)
+  res.status(err.rescode !== undefined ? err.rescode : 418);
   res.json({
-    error: err.message
-  })
-})
+    error: err.message,
+  });
+});
